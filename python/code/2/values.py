25a22545c2760630f71fa1a6e0f5bde26bbd590b
Joel Enrique Esparza Ramirez <joel.030496@gmail.com>
2024-02-01 14:52:58 -0700

    Update python lecture

---
+++ python/code/2/values.py
@@ -0,0 +1,30 @@
+"""
+LECTURE 2
+
+Data type
+"""
+
+# Integers - Whole numbers without a fractional part.
+my_number = 7
+print(type(my_number))
+# Output: <class 'int'>
+
+# Floats - Numbers with a decimal point.
+my_float = 7.0
+print(type(my_float))
+# Output: <class 'float'>
+
+# Strings - A sequence of characters, enclosed in quotes.
+my_string = "Python"
+print(type(my_string))
+# Output: <class 'str'>
+
+# Booleans - Represents True or False.
+my_bool = True
+print(type(my_bool))
+# Output: <class 'bool'>
+
+# NoneType - Represents the absence of a value.
+my_none = None
+print(type(my_none))
+# Output: <class 'NoneType'>
