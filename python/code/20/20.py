d9cff0494ab1b3912e84fc494f2b812de61c21e3
Joel Enrique Esparza Ramirez <joel.030496@gmail.com>
2024-02-27 14:40:48 -0700

    update

---
+++ python/code/20/20.py
@@ -0,0 +1,20 @@
+"""
+Tuples
+"""
+
+my_tuple = (1, 2, 3, 2, 4, 2)
+
+# Count
+count = my_tuple.count(2)  # Counts how many times 2 appears in the tuple
+print(count)  # Output: 3
+
+# Index
+index = my_tuple.index(2)  # Finds the index of the first occurrence of 2
+print(index)  # Output: 1
+
+
+"""
+Tuples are a simple but powerful data structure in Python,
+suitable for grouping together related data that should not
+change throughout the execution of a program.
+"""
