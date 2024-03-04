45b168846c41c2de7b3f0fc137778496125c1dde
Joel Enrique Esparza Ramirez <joel.030496@gmail.com>
2024-03-04 12:26:41 -0700

    rename folder

---
+++ examples/chat/src/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+from flask_socketio import join_room, leave_room, send, SocketIO
+import random
+from string import ascii_uppercase
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "hjhjsdahhds"
+socketio = SocketIO(app)
+
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+    if request.method == "POST":
+        name = request.form.get("name")
+        code = request.form.get("code")
+        join = request.form.get("join", False)
+        create = request.form.get("create", False)
+
+    return render_template("home.html")
+
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
